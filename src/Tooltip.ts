1725504e1cf699f6af4d2f13da8a9efb599c622d
Denis Ivanov <denisx.ivanov@gmail.com>
2018-02-08 11:40:54 +0200

    Added the css classes and the functions to calculate tooltip position

--- src/Tooltip.ts
+++ src/Tooltip.ts
@@ -3,10 +3,12 @@ import TooltipOptions from './Interfaces/TooltipOptions';
 export default class Tooltip {
     guid: string;
     el: HTMLDivElement;
+    // where tooltip div element is going to be appended
+    container: HTMLElement = document.body;
 
     StyleSheets: object = {
-        tooltip_class: "",
-        tooltip_visible_class: ""
+        tooltip_class: "di-tooltip",
+        tooltip_visible_class: "di-tooltip-open"
     };
 
     defaults: TooltipOptions = {
@@ -44,8 +46,82 @@ export default class Tooltip {
         };
     };
 
+    private calcCoordinates(el: HTMLElement) {
+        var box = el.getBoundingClientRect();
+
+        var body = document.body;
+        var docEl = document.documentElement;
+
+        var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+        var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+        var clientTop = docEl.clientTop || body.clientTop || 0;
+        var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+        var top = box.top + scrollTop - clientTop;
+        var left = box.left + scrollLeft - clientLeft;
+
+        return { top: Math.round(top), left: Math.round(left), width: box.width, height: box.height };
+    }
+
+    private getTargetPosition(): object {
+        let el = this.calcCoordinates(this.defaults.target);
+
+        return {
+            top: el.top,
+            left: el.left,
+            w: el.width,
+            h: el.height
+        }
+    }
+
+    private calcOffsets(placement: string, position: object) {
+        let actualHeight = this.el.getBoundingClientRect()["height"], actualWidth = this.el.getBoundingClientRect()["width"];
+
+        switch(placement) {
+            case "top":
+                return {
+                    top: (position["top"] - actualHeight) - 10,
+                    left: position["left"] - position["w"] / 2 - actualWidth / 2
+                }
+            case "left":
+                return {
+                    top: position["top"] + position["height"] / 2 - actualHeight / 2,
+                    left: (position["left"] - actualWidth) - 10
+                }
+            case "bottom":
+                return {
+                    top: (position["top"] + position["height"]) + 10,
+                    left: position["left"] + position["w"] / 2 - actualWidth / 2
+                }
+            case "right":
+                return {
+                    top: position["top"] + position["height"] / 2 - actualHeight / 2,
+                    left: position["left"] + position["w"] + 10
+                }
+        }
+    }
+
     private createElement(): void {
         this.el = document.createElement("div");
+
+        // add the css class
+        this.el.classList.add(this.StyleSheets["di-tooltip"]);
+
+        /**
+         * Get the default or the user preference HTML here
+         */
+        this.el.innerHTML = this.defaults.contentHTML();
+
+        /**
+         * And now appent it before showing up, 
+         * so we can calculate offsetTop based on the height of the element
+         */
+        this.container.appendChild(this.el);
+
+        let placement = this.defaults.position,
+            elPosition = this.getTargetPosition(),
+            calculatedOffsets = this.calcOffsets(placement, elPosition);
     };
 
     private show() {
@@ -57,7 +133,12 @@ export default class Tooltip {
     };
 
     // used by "click" event
-    private toggle() {
+    private toggle(ev: MouseEvent) {
+        ev.stopPropagation();
 
+        // add click event to destroy the tooltip if clicked outside him
+        document.addEventListener("click", ev => {
+            this.destroy();
+        });
     }
 }
