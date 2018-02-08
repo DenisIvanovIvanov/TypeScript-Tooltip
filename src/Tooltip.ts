e3408976d4b3293165021e557b5b838670c03bb3
Denis Ivanov <denisx.ivanov@gmail.com>
2018-02-08 11:10:31 +0200

    Index.css included and added event listeners to the tooltip

--- src/Tooltip.ts
+++ src/Tooltip.ts
@@ -1,34 +1,63 @@
 import TooltipOptions from './Interfaces/TooltipOptions';
 
 export default class Tooltip {
+    guid: string;
+    el: HTMLDivElement;
+
+    StyleSheets: object = {
+        tooltip_class: "",
+        tooltip_visible_class: ""
+    };
 
     defaults: TooltipOptions = {
         target: document.getElementsByTagName("body")[0],
         position: "top",
         trigger: "hover",
-        contentHTML: () => {
+        contentHTML: (): string => {
             return "<p>Test</p>"
         }
-    }
-
-    el: HTMLDivElement;
+    };
 
     constructor(options: TooltipOptions) {
         // override user options with defaults
         this.defaults = Object.assign(this.defaults, options);
 
-        this.show();
-    }
+        // add event listeners to the target
+        this.attachEventListeners();
+    };
 
-    createElement() {
+    private attachEventListeners(): void {
+        let self = this;
+
+        // based on user preferences or the default if no supplied
+        switch(this.defaults.trigger) {
+            case "hover":
+                // on mouse enter show
+                self.defaults.target.addEventListener("mouseenter", self.show);
+                
+                // on mouse leave destroy the tooltip
+                self.defaults.target.addEventListener("mouseleave", self.destroy);
+                break;
+            case "click":
+                self.defaults.target.addEventListener("click", self.toggle);
+                break;
+        };
+    };
+
+    private createElement(): void {
         this.el = document.createElement("div");
-    }
+    };
 
-    show() {
+    private show() {
+        
+    };
 
-    }
+    private destroy() {
 
-    destroy() {
+    };
+
+    // used by "click" event
+    private toggle() {
 
     }
 }
